16b5fc7b2c7436927b23d8aa05947cc7f35ff6c9
stanley <smveme@gmail.com>
2022-06-09 12:14:48 +0100

    add routing

--- medical_center_ui/src/app/app-routing.module.ts
+++ medical_center_ui/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
+import { Call } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CallComponent } from './call/call.component';
 import { ChatPageComponent } from './chat-page/chat-page.component';
 import { MainComponent } from './main/main.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
-  {path: '', component : MainComponent},
-  {path: 'sign-in', component : SignInComponent}
+  {path: 'home', component : MainComponent},
+  {path: '', component : SignInComponent},
+  {path: 'call', component: CallComponent}
 ];
 
 @NgModule({
